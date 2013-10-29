1d67698503c4e5e96612895bbec8f09665d40fb5
Gergely Nemeth <mail@nemethgergely.com>
2013-10-29 15:23:04 +0100

    static

--- app.js
+++ app.js
@@ -2,15 +2,10 @@ var express = require('express'),
     fs = require('fs'),
     app = express();
 
+app.use(express.static(__dirname + '/static'));
+
 app.get('/', function(req, res) {
     res.send('nodeWHAT!?');
-});
-
-app.get('/getpic', function(req, res) {
-    fs.readFile(__dirname + '/static/pic.gif', function (err, data) {
-        if (err) throw err;
-        res.send(data);
-    });
 });
 
 app.listen(3000);
