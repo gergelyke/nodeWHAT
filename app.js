1e5ca807600c893b793304c1a69657502e0a87bd
Gergely Nemeth <mail@nemethgergely.com>
2013-10-29 19:41:55 +0100

    refactor

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ var http = require('http'),
     fs = require('fs'),
     app = express();
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('nodeWHAT!?');
 });
 
 app.get('/file', function (req, res) {
     return fs.readFile(path.join(__dirname, '/static/pic.gif'), function (err, data) {
-        if (err) return console.log(err);
+        if (err) {
+            return console.log(err);
+        }
         return res.send(data);
-    })
+    });
 });
 
 http.createServer(app).listen(3000, function (err) {
-    if (err) return console.log(err);
+    if (err) {
+        return console.log(err);
+    }
 });
