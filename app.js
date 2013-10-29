df41fa7f5ad6913aa78d1bd7af6c08b669d9bc0a
Gergely Nemeth <mail@nemethgergely.com>
2013-10-29 15:24:41 +0100

    middleware

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express'),
     fs = require('fs'),
     app = express();
 
+app.use(function (req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
