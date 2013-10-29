96890c5dd718aac3b8be0e495f250622d1a1249f
Gergely Nemeth <mail@nemethgergely.com>
2013-10-29 15:18:03 +0100

    file read addded

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express'),
+    fs = require('fs'),
     app = express();
 
 app.get('/', function(req, res) {
     res.send('nodeWHAT!?');
+});
+
+app.get('/getpic', function(req, res) {
+    fs.readFile(__dirname + '/static/pic.gif', function (err, data) {
+        if (err) throw err;
+        res.send(data);
+    });
 });
 
 app.listen(3000);
