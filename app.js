73cfe80884226696d7001140ed86456fe9f3326a
gnemeth <gnemeth@GNEMETH-IMAC.local>
2013-10-30 09:28:23 +0100

    http added to master

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-var express = require('express'),
+var http = require('http'),
+    express = require('express'),
     fs = require('fs'),
     app = express();
-
-app.use(function (req, res, next) {
-    console.log(req.method, req.url);
-    next();
-});
 
 app.use(express.static(__dirname + '/static'));
 
@@ -13,4 +9,6 @@ app.get('/', function(req, res) {
     res.send('nodeWHAT!?');
 });
 
-app.listen(3000);
+http.createServer(app).listen(3000, function (err) {
+    if (err) return console.log(err);
+});
